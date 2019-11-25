db1af0b182930a76d5fe93bacb7ce319b2ef22ed
MartinIversen8 <martin-iversen@hotmail.com>
2019-11-25 13:02:17 +0100

    added navigation properties

--- Web-Api_Claimy/Models/CustomerTable.cs
+++ Web-Api_Claimy/Models/CustomerTable.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web_Api_Claimy.Models
 {
     public class CustomerTable
     {
         public int fld_Customer_ID { get; set; }
+        [Required]
         public string fld_Name { get; set; }
         public string fld_Email { get; set; }
         public string fld_Adress { get; set; }
@@ -16,5 +18,7 @@ namespace Web_Api_Claimy.Models
 
         //Foreign Key
         public int fld_Country { get; set; }
+        // Navigation property 
+        public CountryListTable countryList { get; set; }
     }
 }
