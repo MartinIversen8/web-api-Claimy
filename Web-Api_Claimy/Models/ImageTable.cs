39dd466b400637de109640a6da6067acd44e7eb7
MattyxD19 <mathiasbc97@gmail.com>
2019-11-25 12:48:05 +0100

    added classes

---
+++ Web-Api_Claimy/Models/ImageTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Api_Claimy.Models
+{
+    public class ImageTable
+    {
+        public int fld_ID { get; set; }
+        [Required]
+        
+        public Image fld_Image { get; set; }
+
+        public string fld_Description { get; set; }
+
+        //foreign key
+        public int fld_Ticket_ID { get; set; }
+    }
+}
